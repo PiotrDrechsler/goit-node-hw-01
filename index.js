df6d7dc746e0cb9ff9d6ec83438d143774b434a8
Piotr Drechsler <pdrech@gmail.com>
2023-03-04 16:27:48 +0100

    Test
    Create example text data.
    Trying to read those data.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Hello Word!")
+const fs = require('fs').promises;
+
+//czytanie pliku
+const readFile = async () => { 
+    const data = await fs.readFile("./exampleFile.txt")
+    const parsedData = data.toString();
+    console.log(parsedData)
+}
+
+readFile();
